2779d4f345d83dbb6b1abf9036964939c957eb6c
Max2137 <maximuton2006@gmail.com>
2023-02-24 12:59:55 +0100

    Dash
    Dash jest pod shiftem aczkolwiek pierwszy dash wykonany w dowolnym kierunku jest niedokładny, ale pozostałe już są dokładne, nwm czemu (To bug a nie feature)

--- Koloseum/Assets/Dash.cs
+++ Koloseum/Assets/Dash.cs
@@ -2,41 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dash : MonoBehaviour
-{
-
+ public class Dash : MonoBehaviour {
+ 
     public Vector3 moveDirection;
-
-    public const float maxDashTime = 1.0f;
-    public float dashDistance = 10;
-    public float dashStoppingSpeed = 0.1f;
-    float currentDashTime = maxDashTime;
-    float dashSpeed = 6;
-    CharacterController controller;
-
-
-
-    void Awake()
-    {
-        controller = GetComponent<CharacterController>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.LeftControl)) //Right mouse button
-        {
-            currentDashTime = 0;
-        }
-        if (currentDashTime < maxDashTime)
-        {
-            moveDirection = transform.forward * dashDistance;
-            currentDashTime += dashStoppingSpeed;
-        }
-        else
-        {
-            moveDirection = Vector3.zero;
-        }
-        controller.Move(moveDirection * Time.deltaTime * dashSpeed);
-    }
-}
+ 
+     public const float maxDashTime = 1.0f;
+     public float dashDistance = 10;
+     public float dashStoppingSpeed = 0.1f;
+     float currentDashTime = maxDashTime;
+     float dashSpeed = 6;
+     CharacterController controller;
+ 
+     
+ 
+     private void Awake()
+     {
+         controller = GetComponent<CharacterController>();
+     }
+ 
+     // Update is called once per frame
+     void Update () {
+         if (Input.GetKeyDown(KeyCode.LeftShift)) 
+         {
+             currentDashTime = 0;                
+         }
+         if(currentDashTime < maxDashTime)
+         {
+             moveDirection = transform.forward * dashDistance;
+             currentDashTime += dashStoppingSpeed;
+         }
+         else
+         {
+             moveDirection = Vector3.zero;
+         }
+         controller.Move(moveDirection * Time.deltaTime * dashSpeed);
+     }
+ }
+ 
