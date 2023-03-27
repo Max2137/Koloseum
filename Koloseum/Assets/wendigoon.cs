d44fc4190c09e6acfdaa448831834899e010ae77
FranekW <franciszek.wieczorkiewicz@gmail.com>
2023-03-28 00:05:01 +0200

    fsd

--- Koloseum/Assets/wendigoon.cs
+++ Koloseum/Assets/wendigoon.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class wendigoon : MonoBehaviour
 {
+    public float fixedYValue = 1.73f;
+
     public int HP = 5;
     public int maxHP = 5;
 
@@ -17,19 +19,26 @@ public class wendigoon : MonoBehaviour
 
     public Transform player;
 
-    public float dashDistance = 5;
-
     public float preparingCooldown = 60;
+
+    public float dashForce = 1000f;
+    public float dashDuration = 1f;
+    public float dashDistance = 10;
+    private Rigidbody rb;
+
+    public float pushbackForce = 30f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = new Vector3(transform.position.x, fixedYValue, transform.position.z);
+
         if (isFollowing == true)
         {
             transform.LookAt(player);
@@ -52,21 +61,46 @@ public class wendigoon : MonoBehaviour
 
         if (preparingCooldown < 0)
         {
-            isCharging = true;
-
             isPreparing = false;
+
+            isCharging = true;
 
             preparingCooldown = 60;
         }
 
         if (isCharging == true)
         {
-            Vector3 dashDirection = (player.position - transform.position).normalized;
-            transform.Translate(dashDirection * 5 * Time.deltaTime, Space.World);
-
-            Debug.Log("Charging");
+            rb.AddForce(transform.forward * dashForce, ForceMode.Impulse);
 
             isCharging = false;
+
+            Invoke("chargeStop", dashDuration);
         }
     }
+    
+    void chargeStop()
+    {
+        rb.velocity = Vector3.zero;
+
+        isFollowing = true;
+
+        Debug.Log("chargeStop");
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player")) // Check if the other object is the player
+        {
+            Rigidbody otherRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            if (otherRigidbody != null) // Check if the player has a rigidbody component
+            {
+                // Calculate the direction of the pushback effect
+                Vector3 pushbackDirection = other.transform.position - transform.position;
+                pushbackDirection.Normalize();
+
+                // Apply the pushback force to the player
+                otherRigidbody.AddForce(pushbackDirection * pushbackForce, ForceMode.Impulse);
+            }
+        }
+   }
 }
