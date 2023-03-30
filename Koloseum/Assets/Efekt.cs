c244a0570a7ec54fefe70f93c6274afb516ffcf8
Max2137 <maximuton2006@gmail.com>
2023-03-30 19:48:07 +0200

    .

---
+++ Koloseum/Assets/Efekt.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Efekt : MonoBehaviour
+{
+    public Image barefect;
+    public float barLevel;
+
+    public float effect;
+    public float maxefect;
+
+    public float constantDecrease;
+    
+    void Start()
+    {
+         
+    }
+
+   
+    void Update()
+    {
+        barLevel = effect / maxefect;
+        barefect.fillAmount = barLevel;
+
+        effect -= constantDecrease;
+    }
+}
